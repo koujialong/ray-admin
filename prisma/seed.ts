d86203a425ea048f72db8cdad39b976d650085d9
karl <13298172067@wo.cn>
2024-01-18 18:34:18 +0800

    chore: 修改数据库种子脚本

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,50 +8,18 @@ async function main() {
       email: "admin@123.com",
       username: "admin",
       image: null,
-      password: "admin",
-    },
+      password: "admin"
+    }
   ];
   await Promise.all(
     users.map(async (user) => {
       await prisma.user.create({
-        data: user,
+        data: user
       });
-    }),
+    })
   );
 
   const menus: Prisma.MenuCreateInput[] = [
-    {
-      id: "clrg0w3pq0003nw8qujdz6ybe",
-      label: "组件",
-      key: "/main/com",
-      icon: "AppstoreOutlined",
-      parent: null,
-      order: 3,
-    },
-    {
-      id: "clrg0xzvj0004nw8qarow3brk",
-      label: "表格",
-      key: "/main/com/table",
-      icon: "TableOutlined",
-      parent: "clrg0w3pq0003nw8qujdz6ybe",
-      order: 5,
-    },
-    {
-      id: "clrg1c4rb00001675pxkkkko7",
-      label: "编辑器",
-      key: "/main/com/editor",
-      icon: "CreditCardOutlined",
-      parent: "clrg0w3pq0003nw8qujdz6ybe",
-      order: 4,
-    },
-    {
-      id: "clrhfhp4600021675vybummtz",
-      label: "状态管理",
-      key: "/main/com/store",
-      icon: "DatabaseOutlined",
-      parent: "clrg0w3pq0003nw8qujdz6ybe",
-      order: 6,
-    },
     {
       id: "clrhh5o0o00031675y9aktyvw",
       label: "404",
@@ -59,30 +27,8 @@ async function main() {
       icon: "AlertOutlined",
       parent: null,
       order: 11,
-    },
-    {
-      id: "clrhkhx7z0000orkeqpuq6txt",
-      label: "系统管理",
-      key: "/system",
-      icon: "SettingFilled",
-      parent: null,
-      order: 10,
-    },
-    {
-      id: "clrhkj1nk0001orkekm8vurm2",
-      label: "菜单管理",
-      key: "/main/system/menu",
-      icon: "MenuOutlined",
-      parent: "clrhkhx7z0000orkeqpuq6txt",
-      order: 1,
-    },
-    {
-      id: "clrhkjv2p0002orkenu6kmc81",
-      label: "用户管理",
-      key: "/main/system/user",
-      icon: "UserOutlined",
-      parent: "clrhkhx7z0000orkeqpuq6txt",
-      order: 2,
+      menuType: "M",
+      status: "0",
     },
     {
       id: "clrhklkpb0003orke22p701sk",
@@ -91,14 +37,76 @@ async function main() {
       icon: "DashboardOutlined",
       parent: null,
       order: 1,
+      menuType: "M",
+      status: "0",
+    },
+    {
+      id: "clrg0w3pq0003nw8qujdz6ybe",
+      label: "组件",
+      key: "/main/com",
+      icon: "AppstoreOutlined",
+      parent: null,
+      order: 3,
+      menuType: "D",
+      status: "0",
+    },
+    {
+      id: "clrg1c4rb00001675pxkkkko7",
+      label: "编辑器",
+      key: "/main/com/editor",
+      icon: "CreditCardOutlined",
+      parent: "clrg0w3pq0003nw8qujdz6ybe",
+      order: 4,
+      menuType: "M",
+      status: "0",
+    },
+    {
+      id: "clrg0xzvj0004nw8qarow3brk",
+      label: "表格",
+      key: "/main/com/table",
+      icon: "TableOutlined",
+      parent: "clrg0w3pq0003nw8qujdz6ybe",
+      order: 5,
+      menuType: "M",
+      status: "0",
+    },
+    {
+      id: "clrhkj1nk0001orkekm8vurm2",
+      label: "菜单管理",
+      key: "/main/system/menu",
+      icon: "MenuOutlined",
+      parent: "clrhkhx7z0000orkeqpuq6txt",
+      order: 1,
+      menuType: "M",
+      status: "0",
+    },
+    {
+      id: "clrhkjv2p0002orkenu6kmc81",
+      label: "用户管理",
+      key: "/main/system/user",
+      icon: "UserOutlined",
+      parent: "clrhkhx7z0000orkeqpuq6txt",
+      order: 2,
+      menuType: "M",
+      status: "0",
+    },
+    {
+      id: "clrhkhx7z0000orkeqpuq6txt",
+      label: "系统管理",
+      key: "/system",
+      icon: "SettingFilled",
+      parent: null,
+      order: 10,
+      menuType: "D",
+      status: "0",
     },
   ];
   await Promise.all(
     menus.map(async (menu) => {
       await prisma.menu.create({
-        data: menu,
+        data: menu
       });
-    }),
+    })
   );
 }
 

e5fd19662fc92e89956d25e0df999be339670031
寇佳龙 <koujialong@bonc.com.cn>
2024-12-25 15:39:24 +0800

    feat: 添加默认用户名和密码提示

--- src/app/[locale]/(pages)/login/page.tsx
+++ src/app/[locale]/(pages)/login/page.tsx
@@ -75,6 +75,7 @@ export default function Login() {
                 </Button>
               }
             />
+            <div className="text-sm my-2 text-gray-700 dark:text-gray-400">default: admin/admin@123</div>
             <div className="mt-4 flex items-center justify-between">
               <span className="w-1/5 border-b md:w-1/4"></span>
               <a
